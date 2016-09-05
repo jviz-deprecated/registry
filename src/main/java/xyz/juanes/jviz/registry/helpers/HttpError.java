8b6ccc13048df03f0028c47a9b7f562a4368beea
jmjuanes <josemijuanes@gmail.com>
2016-09-05 16:58:02 +0200

    src/main/java/xyz/juanes/jviz/registry/helpers/HttpError.java: manage errors

---
+++ src/main/java/xyz/juanes/jviz/registry/helpers/HttpError.java
@@ -0,0 +1,31 @@
+package xyz.juanes.jviz.registry.helpers;
+
+//Import dependencies
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import java.io.IOException;
+
+//Import config
+import xyz.juanes.jviz.registry.Config;
+
+public class HttpError
+{
+  //Send a new error
+  public static void send(HttpServletResponse response, Integer code, String message) throws IOException
+  {
+    //Generate the new json object
+    JSONObject obj = new JSONObject();
+
+    //Add the message
+    obj.put("message", message);
+
+    //Add the documentation lik
+    obj.put("documentation", Config.documentation);
+
+    //Set the response code
+    response.setStatus(code);
+
+    //Print the response
+    response.getWriter().println(obj.toJSONString());
+  }
+}
