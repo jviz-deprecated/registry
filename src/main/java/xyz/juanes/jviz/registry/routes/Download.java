eed85312e1729542e85ec106dfbf6865540e1ace
jmjuanes <josemijuanes@gmail.com>
2016-09-08 13:18:58 +0200

    src/main/java/xyz/juanes/jviz/registry/routes/Download.java: download module

---
+++ src/main/java/xyz/juanes/jviz/registry/routes/Download.java
@@ -0,0 +1,65 @@
+package xyz.juanes.jviz.registry.routes;
+
+import org.json.simple.JSONObject;
+import xyz.juanes.jviz.registry.Config;
+import xyz.juanes.jviz.registry.firebase.FirebaseREST;
+import xyz.juanes.jviz.registry.firebase.FirebaseResponse;
+import xyz.juanes.jviz.registry.helpers.HttpOut;
+import xyz.juanes.jviz.registry.helpers.SplitUrl;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Download extends HttpServlet
+{
+  //Init function
+  public void init(ServletConfig conf){ Config.initFirebase(); }
+
+  //Download
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+  {
+    //Split the url
+    String[] url = SplitUrl.split(request);
+
+    //Check the length
+    if(url.length != 2)
+    {
+      //Display the error
+      HttpOut.error(response, 400, "Bad request"); return;
+    }
+
+    //Get the module info
+    FirebaseResponse res = FirebaseREST.get("modules/" + url[0]);
+
+    //Check for null or error
+    if(res.isError() || res.isNull())
+    {
+      //Display the error
+      HttpOut.error(response, 500, "Error connecting to the database"); return;
+    }
+
+    //Try reading the information
+    try
+    {
+      //Build the json object
+      JSONObject obj = res.getBodyJSON();
+
+      //Build the download url
+      String download = obj.get("repository") + Config.download.replace("{version}", url[1]);
+
+      //Redirect
+      response.sendRedirect(download);
+    }
+    catch(Exception e)
+    {
+      //Display error
+      HttpOut.error(response, 500, "Error redirecting..."); return;
+    }
+
+    //Exit
+    return;
+  }
+}
